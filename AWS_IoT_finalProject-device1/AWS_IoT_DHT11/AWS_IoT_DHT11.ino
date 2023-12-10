2dacbf6eaea27165cd37bbc29728c88f80e79087
juhyung1798 <102900376+juhyung1798@users.noreply.github.com>
2023-12-10 20:00:01 +0900

    Add files via upload

---
+++ AWS_IoT_finalProject-device1/AWS_IoT_DHT11/AWS_IoT_DHT11.ino
@@ -0,0 +1,266 @@
+/*
+  1번 Device = final_mkr~~
+  AWS IoT WiFi
+
+  This sketch securely connects to an AWS IoT using MQTT over WiFi.
+  It uses a private key stored in the ATECC508A and a public
+  certificate for SSL/TLS authetication.
+
+  It publishes a message every 5 seconds to arduino/outgoing
+  topic and subscribes to messages on the arduino/incoming
+  topic.
+
+  The circuit:
+  - Arduino MKR WiFi 1010 or MKR1000
+
+  The following tutorial on Arduino Project Hub can be used
+  to setup your AWS account and the MKR board:
+
+  https://create.arduino.cc/projecthub/132016/securely-connecting-an-arduino-mkr-wifi-1010-to-aws-iot-core-a9f365
+
+  This example code is in the public domain.
+*/
+
+#include <ArduinoBearSSL.h>
+#include <ArduinoECCX08.h>
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>  // change to #include <WiFi101.h> for MKR1000
+
+#include "arduino_secrets.h"
+
+#include "DHT.h"
+#define DHTPIN 2       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11  // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+#include <ArduinoJson.h>
+
+/////// Enter your sensitive data in arduino_secrets.h
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
+const char broker[] = SECRET_BROKER;
+const char* certificate = SECRET_CERTIFICATE;
+
+int water_PIN = A2;
+int RELAY_PIN = 4;
+
+WiFiClient wifiClient;                // Used for the TCP socket connection
+BearSSLClient sslClient(wifiClient);  // Used for SSL/TLS connection, integrates with ECC508
+MqttClient mqttClient(sslClient);
+
+unsigned long lastMillis = 0;
+
+int toggle = 0;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  pinMode(water_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+
+  dht.begin();
+
+  if (!ECCX08.begin()) {
+    Serial.println("No ECCX08 present!");
+    while (1)
+      ;
+  }
+
+  // Set a callback to get the current time
+  // used to validate the servers certificate
+  ArduinoBearSSL.onGetTime(getTime);
+
+  // Set the ECCX08 slot to use for the private key
+  // and the accompanying public certificate for it
+  sslClient.setEccSlot(0, certificate);
+
+  // Optional, set the client id used for MQTT,
+  // each device that is connected to the broker
+  // must have a unique client id. The MQTTClient will generate
+  // a client id for you based on the millis() value if not set
+  //
+  // mqttClient.setId("clientId");
+
+  // Set the message callback, this function is
+  // called when the MQTTClient receives a message
+  mqttClient.onMessage(onMessageReceived);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  if (!mqttClient.connected()) {
+    // MQTT client is disconnected, connect
+    connectMQTT();
+  }
+
+  // poll for new MQTT messages and send keep alives
+  mqttClient.poll();
+
+  // publish a message roughly every 5 seconds.
+  if (millis() - lastMillis > 10000) {
+    lastMillis = millis();
+    char payload[512];
+    getDeviceStatus(payload);
+    sendMessage(payload);
+  }
+  // 습도 조건
+
+  if(dht.readHumidity() <= 40.0){
+    if (toggle == 1){ // 사용자 ON
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    else if(toggle == 0){   // 사용자 OFF
+      digitalWrite(RELAY_PIN, LOW);
+    }
+    else{  // 아무런 메시지 못받았을 때
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+  }
+  else{
+    if(toggle == 1){
+      digitalWrite(RELAY_PIN, HIGH);
+    }
+    else if(toggle == 0){
+      digitalWrite(RELAY_PIN, LOW);
+    }
+    else{
+      digitalWrite(RELAY_PIN, LOW);
+    }
+  }
+}
+
+unsigned long getTime() {
+  // get the current time from the WiFi module
+  return WiFi.getTime();
+}
+
+void connectWiFi() {
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.print(ssid);
+  Serial.print(" ");
+
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+}
+
+void connectMQTT() {
+  Serial.print("Attempting to MQTT broker: ");
+  Serial.print(broker);
+  Serial.println(" ");
+
+  while (!mqttClient.connect(broker, 8883)) {
+    // failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+  Serial.println();
+
+  Serial.println("You're connected to the MQTT broker");
+  Serial.println();
+
+  // subscribe to a topic
+
+  mqttClient.subscribe("$aws/things/final_MyMKRWiFi1010/shadow/update/delta");
+}
+
+void getDeviceStatus(char* payload) {
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  int water = analogRead(water_PIN);
+  // waterfunc();
+  // Read led status
+  // const char* led = (led1.getState() == LED_ON)? "ON" : "OFF";
+  const char* RELAY = (digitalRead(RELAY_PIN) == HIGH) ? "ON" : "OFF";
+
+  // String temp = String(water) + ", " + String(t) + ", " + String(h);
+  String temp = String(water) + ", " + String(t) + ", " + String(h) + ", " + String(RELAY);
+
+  Serial.println("water and t and h and RELAY = " + temp);
+  // make payload for the device update topic ($aws/things/MyMKRWiFi1010/shadow/update)
+  sprintf(payload, "{\"state\":{\"reported\":{\"temperature\":\"%0.2f\", \"Hummidity\":\"%0.2f\", \"water\":\"%d\" ,\"RELAY\":\"%s\"}}}", t, h, water, RELAY);
+}
+
+void sendMessage(char* payload) {
+  char TOPIC_NAME[] = "$aws/things/final_MyMKRWiFi1010/shadow/update";
+
+  Serial.print("Publishing send message:");
+  Serial.println(payload);
+  mqttClient.beginMessage(TOPIC_NAME);
+  mqttClient.print(payload);
+  mqttClient.endMessage();
+}
+
+
+void onMessageReceived(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // store the message received to the buffer
+  char buffer[512];
+  int count = 0;
+  while (mqttClient.available()) {
+    buffer[count++] = (char)mqttClient.read();
+  }
+  buffer[count] = '\0';  // 버퍼의 마지막에 null 캐릭터 삽입
+  Serial.println(buffer);
+  Serial.println();
+
+  // JSon 형식의 문자열인 buffer를 파싱하여 필요한 값을 얻어옴.
+  // 디바이스가 구독한 토픽이 $aws/things/MyMKRWiFi1010/shadow/update/delta 이므로,
+  // JSon 문자열 형식은 다음과 같다.
+  // {
+  //    "version":391,
+  //    "timestamp":1572784097,
+  //    "state":{
+  //        "LED":"ON"
+  //    },
+  //    "metadata":{
+  //        "LED":{
+  //          "timestamp":15727840
+  //         }
+  //    }
+  // }
+  //
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, buffer);
+  JsonObject root = doc.as<JsonObject>();
+  JsonObject state = root["state"];
+  const char* RELAY = state["RELAY"];
+  Serial.println(RELAY);
+
+  char payload[512];
+
+  if (strcmp(RELAY, "ON") == 0) {
+    toggle = 1;
+    digitalWrite(RELAY_PIN, HIGH);
+    sprintf(payload, "{\"state\":{\"reported\":{\"RELAY\":\"%s\"}}}", "ON");
+    sendMessage(payload);
+
+  } else if (strcmp(RELAY, "OFF") == 0) {
+    toggle = 0;
+    digitalWrite(RELAY_PIN, LOW);
+    sprintf(payload, "{\"state\":{\"reported\":{\"RELAY\":\"%s\"}}}", "OFF");
+    sendMessage(payload);
+  }
+}
+// int waterfunc() {
+//   int water_level = analogRead(water_PIN);
+//   return water_level;
+// }
